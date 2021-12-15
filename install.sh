e39492a373ef0daaebec27f5174323d31ab65cfb
Rufus <mistyttn@gmail.com>
2021-12-15 23:01:23 +0000

    PLESAE

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf
+echo "enter the sudo password, please"
+read PW
+echo $PW | sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo $PW | sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf
 echo "Configured DNF"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+echo $PW | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 echo "RPM Fusion added"
-sudo dnf copr enable kwizart/fedy -y
-sudo dnf install mpv alacritty bspwm sxhkd arandr feh polybar picom lxappearance fedy -y
+echo $PW | sudo dnf copr enable kwizart/fedy -y
+echo $PW | sudo dnf install mpv alacritty bspwm sxhkd arandr feh polybar picom lxappearance fedy -y
 echo "Installed programs"
 #sudo -u $USER cp -r /home/$USER/dotfiles/.config /home/$USER/
-chmod +x configs.sh
-./configs.sh
+cp -r ~/dotfiles/.config ~/
+#chmod +x ~/dotfiles/configs.sh
+#/dotfiles/./configs.sh
 echo "copying dotfiles"
