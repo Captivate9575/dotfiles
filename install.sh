b56a3ffc1902273d04ac8be3e089a4bfd6c2d9f9
Rufus <mistyttn@gmail.com>
2021-12-15 21:46:44 +0000

    more stuff

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
 sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf
-
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf copr enable kwizart/fedy
+echo "Configured DNF"
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+echo "RPM Fusion added"
+sudo dnf copr enable kwizart/fedy -y
 sudo dnf install mpv alacritty bspwm sxhkd arandr feh polybar picom lxappearance fedy -y
+echo "Installed programs"
 cp ~/dotfiles/configs ~/.config/
+echo "Copied configs, everything is done!"
